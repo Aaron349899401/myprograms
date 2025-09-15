24131ea9a178ef4823020d0af78c3363a6e83f04
Aaron349899401 <aarongoogoo292gmail.com>
2025-09-15 10:59:18 -0400

    skibid ohio rizz

---
+++ p1.py
@@ -0,0 +1,8 @@
+# My first program
+# Created on 09/15/2025
+# Purpose: to learn python
+# Editor: Aaron Huang
+name = input("Hey there, can I have your name?: ")
+age = input("can I have your age")
+
+print(f"Hi {name}, you are {age} years old")
