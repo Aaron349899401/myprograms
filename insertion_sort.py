dbf4bc5aa1709eef368d97b611f8bb5450791bd8
unknown <aarongoogoo29@gmail.com>
2025-11-05 10:34:54 -0500

    completed

--- insertion_sort.py
+++ insertion_sort.py
@@ -10,3 +10,9 @@ def insertion(a_list):
             j -= 1
 
         i += 1
+
+def insertion_for(a_list):
+    for i in range(1, len(a_list)):
+        for money in range(i, 0, -1):
+            if a_list[money] < a_list[money-1]:
+                a_list[money-1], a_list[money] = a_list[money], a_list[money-1]
