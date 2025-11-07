04ad15a0d62828322be4ddb8b7e453abe744cd41
unknown <aarongoogoo29@gmail.com>
2025-11-07 10:20:18 -0500

    completed

--- problem.py
+++ problem.py
@@ -1,7 +1,51 @@
 wow_list = [1, 1, 2, 3, 4, 5, 6, 9]
 target = 6
-result = any(
-    (lambda x: target - x in wow_list)(x)
-    for x in wow_list
-)
-print(result)
+
+# method 2
+def poop(wow_list, target):
+    for x in range(len(wow_list)-1):
+        for y in range(x+1, len(wow_list)):
+            if x + y == target:
+                return True
+    return False
+# print(poop(wow_list, target))
+
+# method 3
+def lin(wow_list, target):
+    for value in range(len(wow_list)-1):
+        diff = target - wow_list[value]
+        for j in range(i+1, len(wow_list)):
+            if wow_list[j] == diff:
+                return True
+
+# method 3.5
+def lin2(wow_list, target):
+    for x in wow_list:
+        if x != target/2 and any(lambda x: target - x in wow_list)(x):
+            return True
+
+# method 3.75
+def lin3(wow_list, target):
+    result = any(
+        (lambda x: target - x in wow_list)(x)
+        for x in wow_list
+    )
+    return result
+
+# print(lin2(wow_list, target))
+
+# method 5
+def meth5(wow_list, target):
+    h = len(wow_list)-1
+    l = 0
+    while l < h:
+        total = wow_list[l] + wow_list[h]
+        if target == total:
+            return True
+        else:
+            if total < target:
+                l += 1
+            else:
+                h -= 1
+    return False
+print(meth5(wow_list, target))
--- selection_sort.py
+++ selection_sort.py
@@ -1,32 +1,35 @@
-def among(us_list):
-    if len(us_list) <= 1:
-        return us_list
-    else:
-        i = 0
-        while i < len(us_list):
-            smallest = us_list[i] # then prove it is the smallest or is not
-            j = i + 1
-            new_location = i
-            while j < len(us_list):
-                new_value = us_list[j]
-                if new_value < smallest:
-                    smallest = new_value
-                    new_location = j
-                j += 1
-            us_list[i], us_list[new_location] = us_list[new_location], us_list[i]
-            i += 1
+# def among(us_list):
+#     if len(us_list) <= 1:
+#         return us_list
+#     else:
+#         i = 0
+#         while i < len(us_list):
+#             smallest = us_list[i] # then prove it is the smallest or is not
+#             j = i + 1
+#             new_location = i
+#             while j < len(us_list):
+#                 new_value = us_list[j]
+#                 if new_value < smallest:
+#                     smallest = new_value
+#                     new_location = j
+#                 j += 1
+#             us_list[i], us_list[new_location] = us_list[new_location], us_list[i]
+#             i += 1
 
-# for loop version
-def selection_sort(arr):
-    n = len(arr)
-    for i in range(n-1):
-        max_index = i
-        for j in range(i + 1, n):
-            if arr[j] > arr[max_index]:
-                max_index = j
-        arr[i], arr[max_index] = arr[max_index], arr[i]
-    return arr
+# # for loop version
+# def selection_sort(arr):
+#     n = len(arr)
+#     for i in range(n-1):
+#         max_index = i
+#         for j in range(i + 1, n):
+#             if arr[j] > arr[max_index]:
+#                 max_index = j
+#         arr[i], arr[max_index] = arr[max_index], arr[i]
+#     return arr
 
-ar = input()
-arr = [x.strip() for x in ar.split(",")]
-print(selection_sort(arr))
+# print(selection_sort(arr))
+
+def wow(whoa):
+    return [str(x for x in whoa)]
+whoa = [1, 2, 3, 4, 5]
+print(wow(whoa))
